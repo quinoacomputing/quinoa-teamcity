6ac0f2445e8c5e56e50976f86c68e82e5a22d278
Jozsef Bakosi <jbakosi@lanl.gov>
2017-05-01 08:40:33 -0600

    Change id so it does not conflict with quinoa-kotlin repo

--- .teamcity/Quinoa_2/vcsRoots/Quinoa_2_GitGithubComQuinoacomputingQuinoaTeamcityGitRefsHeadsMaster.kt
+++ .teamcity/Quinoa_2/vcsRoots/Quinoa_2_GitGithubComQuinoacomputingQuinoaTeamcityGitRefsHeadsMaster.kt
@@ -4,7 +4,7 @@ import jetbrains.buildServer.configs.kotlin.v10.*
 import jetbrains.buildServer.configs.kotlin.v10.vcs.GitVcsRoot
 
 object Quinoa_2_GitGithubComQuinoacomputingQuinoaTeamcityGitRefsHeadsMaster : GitVcsRoot({
-    uuid = "39847b99-5323-4453-aa17-eaed879042a6"
+    uuid = "39847b99-5323-4453-aa17-eaed879142a6"
     extId = "Quinoa_2_GitGithubComQuinoacomputingQuinoaTeamcityGitRefsHeadsMaster"
     name = "git@github.com:quinoacomputing/quinoa-teamcity.git#refs/heads/master"
     url = "git@github.com:quinoacomputing/quinoa-teamcity.git"
