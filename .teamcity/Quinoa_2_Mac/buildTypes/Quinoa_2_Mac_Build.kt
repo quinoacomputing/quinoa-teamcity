1b5f378dc966a4a53b5783027e11eb3560729321
Jozsef Bakosi <jbakosi@lanl.gov>
2017-07-21 06:52:19 -0600

    Enable select ROOT builds on Mac

--- .teamcity/Quinoa_2_Mac/buildTypes/Quinoa_2_Mac_Build.kt
+++ .teamcity/Quinoa_2_Mac/buildTypes/Quinoa_2_Mac_Build.kt
@@ -9,16 +9,18 @@ class Quinoa_2_Mac_Build( bp: BuildParams ) : BuildType({
 
     val paramToId = bp.buildtype.toString().toExtId() +
                     bp.compiler.toString().toExtId() +
+                    bp.root.toString().toExtId() +
                     bp.rndq.toString().toExtId();
 
     uuid = "7df011b4-4795-4b89-9d03-aba1b1cb53f7_$paramToId"
     extId = "Quinoa_2_Mac_Build_$paramToId"
-    name = "${bp.buildtype.toString()}, ${bp.compiler.toString()}, rndq=${bp.rndq.toString()}"
+    name = "${bp.buildtype.toString()}, ${bp.compiler.toString()}, root=${bp.root.toString()}, rndq=${bp.rndq.toString()}"
     description = "Mac matrix build instance"
 
     params {
         param("buildtype", bp.buildtype.toString())
         param("compiler", bp.compiler.toString())
+        param("root", bp.root.toString())
         param("rndq", bp.rndq.toString())
     }
 })
