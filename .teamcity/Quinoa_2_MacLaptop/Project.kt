d9799e2bddb46ddd98d566eb0b17268e51eab325
Jozsef Bakosi <jbakosi@lanl.gov>
2017-07-21 07:07:06 -0600

    Add Mac laptop builds

---
+++ .teamcity/Quinoa_2_MacLaptop/Project.kt
@@ -0,0 +1,40 @@
+package Quinoa_2_MacLaptop
+
+import Quinoa_2_MacLaptop.buildTypes.*
+import Quinoa_2_MacLaptop.buildParams.*
+import jetbrains.buildServer.configs.kotlin.v10.*
+import jetbrains.buildServer.configs.kotlin.v10.Project
+
+object Project : Project({
+    uuid = "7b48bd08-3540-4fa1-ac86-d238159396f3"
+    extId = "Quinoa_2_MacLaptop"
+    parentId = "Quinoa_2"
+    name = "MacLaptop"
+    description = "Mac laptop builds"
+
+    template(Quinoa_2_MacLaptop_Matrix)
+    val allBuilds = mutableListOf< BuildParams >()
+
+    // Generate matrix with all possible combinations of build parameters
+    // without ROOT, defined in package buildParams.
+    CmakeBuildType.values().forEach{ b ->
+      Compiler.values().forEach{ c ->
+        for( r in listOf( true, false ) ) {
+          allBuilds.add( BuildParams(b,c,false,r) )
+        }
+      }
+    }
+
+    // Add ROOT builds
+    //allBuilds.add( BuildParams( CmakeBuildType.Debug, Compiler.clang, false, true ) )
+
+    val builds = mutableListOf< BuildParams >()
+
+    // Optionally exclude some builds
+    allBuilds.forEach{ b ->
+      builds.add( b );
+    }
+
+    // Generate TeamCity builds
+    builds.forEach{ buildType( Quinoa_2_MacLaptop_Build(it) ) }
+})
---
+++ .teamcity/Quinoa_2_MacLaptop/buildTypes/Quinoa_2_MacLaptop_BuildParams.kt
@@ -0,0 +1,9 @@
+package Quinoa_2_MacLaptop.buildParams
+
+enum class CmakeBuildType { Debug, Release }
+enum class Compiler { clang, gnu }
+
+data class BuildParams( val buildtype: CmakeBuildType,
+                        val compiler: Compiler,
+                        val root: Boolean,
+                        val rndq: Boolean )
