f48e317c01f220bae4981661ab254baecef423c1
Jozsef Bakosi <jbakosi@lanl.gov>
2018-02-17 07:42:21 -0700

    Remove ccscs3 requirement from doc deploy

--- .teamcity/Quinoa_2_Doc/buildTypes/Quinoa_2_Doc_Deploy.kt
+++ .teamcity/Quinoa_2_Doc/buildTypes/Quinoa_2_Doc_Deploy.kt
@@ -82,6 +82,5 @@ object Quinoa_2_Doc_Deploy : BuildType({
 
     requirements {
         equals("teamcity.agent.jvm.os.name", "Linux")
-        contains("teamcity.agent.name", "ccscs3-14")
     }
 })
