5a75b87a09d94bbe1fdf3156588d995be7bf44f4
Jozsef Bakosi <jbakosi@lanl.gov>
2018-02-21 12:47:17 -0700

    Remove Mac

--- .teamcity/Quinoa_2_Mac/buildTypes/Quinoa_2_Mac_BuildParams.kt
+++
@@ -1,9 +0,0 @@
-package Quinoa_2_Mac.buildParams
-
-enum class CmakeBuildType { Debug, Release }
-enum class Compiler { clang, gnu }
-
-data class BuildParams( val buildtype: CmakeBuildType,
-                        val compiler: Compiler,
-                        val rndq: Boolean,
-                        val tpl: String )
