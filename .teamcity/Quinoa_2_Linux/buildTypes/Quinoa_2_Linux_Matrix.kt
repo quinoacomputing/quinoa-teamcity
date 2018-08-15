90afc6e4036312cc70dc2452a24866608c1b0c8f
Jozsef Bakosi <jbakosi@lanl.gov>
2018-08-15 09:24:51 -0600

    Remove ninja module from linux builds

--- .teamcity/Quinoa_2_Linux/buildTypes/Quinoa_2_Linux_Matrix.kt
+++ .teamcity/Quinoa_2_Linux/buildTypes/Quinoa_2_Linux_Matrix.kt
@@ -27,7 +27,7 @@ object Quinoa_2_Linux_Matrix : Template({
       [ %rndq% == true ] && module load charm-rndq/%compiler%-%stdlibcpp%
       [ %rndq% == false ] && module load charm/%compiler%-%stdlibcpp%
       module load h5part/%compiler% trilinos/%compiler%-%stdlibcpp%/%mathlib% omega_h/%compiler%-%stdlibcpp%
-      module load pugixml pegtl pstreams boost-1.65.1-gcc-4.8.5-s7d4zmv gmsh-3.0.6-gcc-4.8.5-n34xdqr random123 tut numdiff libc++ backward-cpp highwayhash brigand ninja-1.8.2-gcc-4.8.5-srfy2lo
+      module load pugixml pegtl pstreams boost-1.65.1-gcc-4.8.5-s7d4zmv gmsh-3.0.6-gcc-4.8.5-n34xdqr random123 tut numdiff libc++ backward-cpp highwayhash brigand
       module list""".trimIndent()
 
     steps {
