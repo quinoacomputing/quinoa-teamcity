aad65b967ead1b65a59ef1b4152b84b9bab27b54
Jozsef Bakosi <jbakosi@lanl.gov>
2018-08-15 09:23:10 -0600

    Switch back to make for linux builds

--- .teamcity/Quinoa_2_Linux/buildTypes/Quinoa_2_Linux_Matrix.kt
+++ .teamcity/Quinoa_2_Linux/buildTypes/Quinoa_2_Linux_Matrix.kt
@@ -42,7 +42,7 @@ object Quinoa_2_Linux_Matrix : Template({
             scriptContent = """
                 ${stepPrefix}
                 rm -rf build && mkdir build && cd build
-                cmake -DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_C_COMPILER=mpicc -DCMAKE_BUILD_TYPE=%buildtype% -DSTDLIBCPP=%stdlibcpp% -DCMAKE_DISABLE_FIND_PACKAGE_RNGSSE2=!%rngsse2% -DCMAKE_DISABLE_FIND_PACKAGE_TestU01=!%testu01% -DCMAKE_CXX_FLAGS=-Werror -DCMAKE_CXX_COMPILER_LAUNCHER=ccache-swig -GNinja ../src && ccache-swig -z && ninja -j16 && ccache-swig -s
+                cmake -DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_C_COMPILER=mpicc -DCMAKE_BUILD_TYPE=%buildtype% -DSTDLIBCPP=%stdlibcpp% -DCMAKE_DISABLE_FIND_PACKAGE_RNGSSE2=!%rngsse2% -DCMAKE_DISABLE_FIND_PACKAGE_TestU01=!%testu01% -DCMAKE_CXX_FLAGS=-Werror -DCMAKE_CXX_COMPILER_LAUNCHER=ccache-swig ../src && ccache-swig -z && make -j16 && ccache-swig -s
             """.trimIndent()
         }
         script {
