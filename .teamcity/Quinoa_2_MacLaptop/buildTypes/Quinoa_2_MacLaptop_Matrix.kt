2d938eb7e06f93d612497f67d2f115b232449cd8
Jozsef Bakosi <jbakosi@lanl.gov>
2018-09-20 09:18:53 -0600

    Use ninja for mac laptop builds

--- .teamcity/Quinoa_2_MacLaptop/buildTypes/Quinoa_2_MacLaptop_Matrix.kt
+++ .teamcity/Quinoa_2_MacLaptop/buildTypes/Quinoa_2_MacLaptop_Matrix.kt
@@ -31,8 +31,7 @@ object Quinoa_2_MacLaptop_Matrix : Template({
             scriptContent = """
                 ${stepPrefix}
                 rm -rf build && mkdir build && cd build
-                cmake -DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_C_COMPILER=mpicc -DCMAKE_BUILD_TYPE=%buildtype% -DCMAKE_CXX_FLAGS=-Werror -DTPL_DIR=/Users/jbakosi/code/quinoa-tpl/install/%compiler%-x86_64%tpl% -DENABLE_ROOT=%root% -DRUNNER_ARGS="-oversubscribe --bind-to none --map-by node" ../src
-                make -j%teamcity.agent.hardware.cpuCount%
+                cmake -DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_C_COMPILER=mpicc -DCMAKE_BUILD_TYPE=%buildtype% -DCMAKE_CXX_FLAGS=-Werror -DTPL_DIR=/Users/jbakosi/code/quinoa-tpl/install/%compiler%-x86_64%tpl% -DENABLE_ROOT=%root% -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -GNinja ../src && ccache -z && ninja && ccache -s
             """.trimIndent()
         }
         script {
@@ -41,7 +40,7 @@ object Quinoa_2_MacLaptop_Matrix : Template({
             workingDir = "build"
             scriptContent = """
                 ${stepPrefix}
-                ../script/run_tests.sh 8 -oversubscribe
+                ../script/run_tests.sh %teamcity.agent.hardware.cpuCount% -oversubscribe
             """.trimIndent()
         }
     }
