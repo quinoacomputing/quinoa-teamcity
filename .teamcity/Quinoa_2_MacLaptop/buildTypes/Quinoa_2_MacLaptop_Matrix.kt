c1730b6ca28d0476e59ea8a6fb7f506431970abe
Jozsef Bakosi <jbakosi@lanl.gov>
2018-11-23 10:26:55 -0700

    Correct runner args in mac laptop builds

--- .teamcity/Quinoa_2_MacLaptop/buildTypes/Quinoa_2_MacLaptop_Matrix.kt
+++ .teamcity/Quinoa_2_MacLaptop/buildTypes/Quinoa_2_MacLaptop_Matrix.kt
@@ -31,7 +31,7 @@ object Quinoa_2_MacLaptop_Matrix : Template({
             scriptContent = """
                 ${stepPrefix}
                 rm -rf build && mkdir build && cd build
-                cmake -DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_C_COMPILER=mpicc -DCMAKE_BUILD_TYPE=%buildtype% -DCMAKE_CXX_FLAGS=-Werror -DTPL_DIR=/Users/jbakosi/code/quinoa-tpl/install/%compiler%-x86_64%tpl% -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -GNinja ../src && ccache -z && ninja && ccache -s
+                cmake -DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_C_COMPILER=mpicc -DCMAKE_BUILD_TYPE=%buildtype% -DCMAKE_CXX_FLAGS=-Werror -DTPL_DIR=/Users/jbakosi/code/quinoa-tpl/install/%compiler%-x86_64%tpl% -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -GNinja ../src && ccache -z && ninja -j%teamcity.agent.hardware.cpuCount% && ccache -s
             """.trimIndent()
         }
         script {
@@ -40,7 +40,7 @@ object Quinoa_2_MacLaptop_Matrix : Template({
             workingDir = "build"
             scriptContent = """
                 ${stepPrefix}
-                ../script/run_tests.sh %teamcity.agent.hardware.cpuCount% -oversubscribe
+                ../script/run_tests.sh %teamcity.agent.hardware.cpuCount% ./charmrun +p -oversubscribe
             """.trimIndent()
         }
     }
