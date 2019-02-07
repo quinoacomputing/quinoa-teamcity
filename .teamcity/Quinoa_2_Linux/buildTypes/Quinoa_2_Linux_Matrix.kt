6b82d58cd73beeefa0055148afe701a163519669
Jozsef Bakosi <jbakosi@lanl.gov>
2019-02-07 05:13:01 -0700

    Play with linux test cmd

--- .teamcity/Quinoa_2_Linux/buildTypes/Quinoa_2_Linux_Matrix.kt
+++ .teamcity/Quinoa_2_Linux/buildTypes/Quinoa_2_Linux_Matrix.kt
@@ -52,7 +52,7 @@ object Quinoa_2_Linux_Matrix : Template({
             workingDir = "build"
             scriptContent = """
                 ${stepPrefix}
-                [ %smp% ] && ./charmrun +p 6 "--bind-to none -oversubscribe" Main/unittest -v -q +ppn 3 || mpirun -n 2 "--bind-to none -oversubscribe" Main/unittest -v -q && ctest -j8 --output-on-failure -LE extreme
+                if [ %smp% ]; then ./charmrun +p 6 "--bind-to none -oversubscribe" Main/unittest -v -q +ppn 3; else mpirun -n 2 "--bind-to none -oversubscribe" Main/unittest -v -q; fi && ctest -j8 --output-on-failure -LE extreme
             """.trimIndent()
         }
     }
