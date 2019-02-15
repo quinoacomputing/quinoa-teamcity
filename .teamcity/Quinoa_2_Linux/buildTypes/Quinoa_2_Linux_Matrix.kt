e90e7ea2f0342d00a394c83e6e77452ef5b179a7
Jozsef Bakosi <jbakosi@lanl.gov>
2019-02-14 20:25:03 -0700

    Add ccache to linux builds

--- .teamcity/Quinoa_2_Linux/buildTypes/Quinoa_2_Linux_Matrix.kt
+++ .teamcity/Quinoa_2_Linux/buildTypes/Quinoa_2_Linux_Matrix.kt
@@ -28,7 +28,7 @@ object Quinoa_2_Linux_Matrix : Template({
       [[ %rndq% == false && %smp% == true ]] && module load charm-smp/%compiler%-%stdlibcpp%
       [[ %rndq% == false && %smp% == false ]] && module load charm/%compiler%-%stdlibcpp%
       module load h5part/%compiler% trilinos/%compiler%-%stdlibcpp%/%mathlib% omega_h/%compiler%-%stdlibcpp%
-      module load pugixml pegtl pstreams boost-1.65.1-gcc-4.8.5-s7d4zmv gmsh-3.0.6-gcc-4.8.5-n34xdqr ninja-1.8.2-gcc-4.8.5-srfy2lo random123 tut numdiff libc++ backward-cpp highwayhash brigand
+      module load pugixml pegtl pstreams boost-1.65.1-gcc-4.8.5-s7d4zmv gmsh-3.0.6-gcc-4.8.5-n34xdqr ninja-1.8.2-gcc-4.8.5-srfy2lo random123 tut numdiff libc++ backward-cpp highwayhash brigand ccache-3.3.4-gcc-4.8.5-3rwbf2t
       module list""".trimIndent()
 
     steps {
@@ -43,7 +43,7 @@ object Quinoa_2_Linux_Matrix : Template({
             scriptContent = """
                 ${stepPrefix}
                 rm -rf build && mkdir build && cd build
-                cmake -DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_C_COMPILER=mpicc -DCMAKE_BUILD_TYPE=%buildtype% -DSTDLIBCPP=%stdlibcpp% -DCMAKE_DISABLE_FIND_PACKAGE_RNGSSE2=!%rngsse2% -DCMAKE_DISABLE_FIND_PACKAGE_TestU01=!%testu01% -DCMAKE_CXX_FLAGS=-Werror -DRUNNER_ARGS="--bind-to none -oversubscribe" -GNinja ../src && ninja -j8
+                cmake -DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_C_COMPILER=mpicc -DCMAKE_BUILD_TYPE=%buildtype% -DSTDLIBCPP=%stdlibcpp% -DCMAKE_DISABLE_FIND_PACKAGE_RNGSSE2=!%rngsse2% -DCMAKE_DISABLE_FIND_PACKAGE_TestU01=!%testu01% -DCMAKE_CXX_FLAGS=-Werror -DRUNNER_ARGS="--bind-to none -oversubscribe" -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -GNinja ../src && ccache -z && ninja -j8 && ccache -s
             """.trimIndent()
         }
         script {
