0d1500867f2eea437fdd627ffc7b217bd45ba48d
Jozsef Bakosi <jbakosi@lanl.gov>
2019-08-08 15:14:20 -0600

    Load gcc 8.2 module for intel to pick up 8.2 headers

--- .teamcity/Quinoa_2_Linux/buildTypes/Quinoa_2_Linux_Matrix.kt
+++ .teamcity/Quinoa_2_Linux/buildTypes/Quinoa_2_Linux_Matrix.kt
@@ -18,7 +18,7 @@ object Quinoa_2_Linux_Matrix : Template({
       . ${'$'}SPACK_ROOT/share/spack/setup-env.sh
       [ %compiler% == clang ] && module load clang/9 openmpi/3.1.3/clang/9
       [ %compiler% == gnu ] && module load gcc-9.1.0-gcc-8.2.0-757ly4p openmpi-3.1.3-gcc-9.1-3h7qsmb root/gnu-9
-      [ %compiler% == intel ] && module load intel/2019 mpi
+      [ %compiler% == intel ] && module load gcc-8.2.0-gcc-4.8.5-53nwzse intel/2019 mpi
       [ %stdlibcpp% == libc++ ] && module load libc++-clang-9 
       [ %mathlib% == mkl ] && module load mkl/2019
       [[ %mathlib% == lapack && %compiler% == intel ]] && module load lapack/intel
