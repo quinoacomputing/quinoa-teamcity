333ec325498066930aaf1c6e88638178cdd5cec6
Jozsef Bakosi <jbakosi@lanl.gov>
2021-01-19 19:08:04 -0700

    Fix doc build cmd for lagrange

--- .teamcity/Quinoa_2_Doc/buildTypes/Quinoa_2_Doc_Matrix.kt
+++ .teamcity/Quinoa_2_Doc/buildTypes/Quinoa_2_Doc_Matrix.kt
@@ -20,15 +20,15 @@ object Quinoa_2_Doc_Matrix : Template({
         rm -rf build && mkdir build && cd build
         """.trimIndent()
 
-    val cmakeCmd = "cmake -DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_C_COMPILER=mpicc -DCMAKE_BUILD_TYPE=%buildtype% -DCMAKE_CXX_FLAGS=-Werror -DCOVERAGE=on -DRUNNER_ARGS=\"--bind-to none --map-by node -oversubscribe\" -DTPL_DIR=/scratch3/jbakosi/quinoa-tpl/gnu ../src"
+    val cmakeCmd = "cmake -DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_C_COMPILER=mpicc -DCMAKE_BUILD_TYPE=%buildtype% -DCMAKE_CXX_FLAGS=-Werror -DCOVERAGE=on -DRUNNER_ARGS=\"--bind-to none -oversubscribe\" -DTPL_DIR=/scratch3/jbakosi/quinoa-tpl/gnu ../src"
 
-    val makeCmd = "make -j40"
+    val makeCmd = "make -j36"
 
     steps {
         script {
             name = "Verify commit"
             id = "RUNNER_17"
-            scriptContent = """/ccs/opt/git/bin/git verify-commit %build.vcs.number% 2>&1 | grep "Good signature""""
+            scriptContent = """git verify-commit %build.vcs.number% 2>&1 | grep "Good signature""""
         }
         script {
             name = "Generate unit test coverage"
